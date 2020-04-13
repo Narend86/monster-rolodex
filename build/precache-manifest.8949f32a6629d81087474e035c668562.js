19868cd4348f363311e0c2e26203f3660e0ce48f
narendra <narendra@bitcot.com>
2020-04-13 22:17:50 +0530

    deploy on github

---
+++ build/precache-manifest.8949f32a6629d81087474e035c668562.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5345d1148473ef85ab5aba053972b0c4",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "c35c128cdfca9d2a64a7",
+    "url": "/monster-rolodex/static/css/main.9ee37e0f.chunk.css"
+  },
+  {
+    "revision": "bd4da6ab1d9e405c24fb",
+    "url": "/monster-rolodex/static/js/2.e552c0ac.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rolodex/static/js/2.e552c0ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c35c128cdfca9d2a64a7",
+    "url": "/monster-rolodex/static/js/main.403a471e.chunk.js"
+  },
+  {
+    "revision": "6ff75bf61634f838a33d",
+    "url": "/monster-rolodex/static/js/runtime-main.354f7ca7.js"
+  }
+]);
